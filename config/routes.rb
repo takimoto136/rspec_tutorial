d590faf21eb488ab0c2ebdd18a001eb951737696
takimoto <takimoto@lassic.co.jp>
2013-12-16 18:14:47 +0900

    Add question page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 RspecTutorial::Application.routes.draw do
   match "menue", to: 'menue#index', 'via' => :get, as: 'menue'
+  match "question/:id", to: 'question/#index', 'via' => :get, as: 'question'
+  match "question/:id", to: 'question/#create', 'via' => :post, as: 'question'
+  match "question/:id", to: 'question/#show', 'via' => :get, as: 'question_ans'
 end
